b39d34342e6c0fccc3110d875e5a6f6fedc41c5f
max <obregonjefferson2@gmail.com>
2024-10-01 20:17:16 -0500

    añadi metadatas al layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 import Navbar from './components/Navbar';
 import './globals.css';
+import Head from 'next/head';
 
 
 export const metadata = {
@@ -15,8 +16,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-      </head>
+      <Head>
+        {/* Título de la página */}
+        <title>CertificaCursos - Cursos online con certificados</title>
+        
+        {/* Metadatos generales */}
+        <meta name="description" content="Ofrecemos una amplia variedad de cursos online con la posibilidad de obtener certificados al finalizar." />
+        <meta name="keywords" content="cursos online, certificados, educación en línea, certificación de cursos, aprender en línea , consultinggroup" />
+        <meta name="author" content="CertificaCursos" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+
+        {/* Open Graph para redes sociales */}
+        <meta property="og:title" content="CertificaCursos - Cursos online con certificados" />
+        <meta property="og:description" content="Obtén tu certificado en cualquier curso que elijas al completar las lecciones. Inscríbete hoy." />
+        <meta property="og:image" content="/img/grupo-consultinggroup.jpg" />
+        <meta property="og:url" content="https://www.facebook.com/groupconsultingperu" />
+        <meta property="og:type" content="https://consultinggroup.com.pe" />
+
+
+
+        {/* Favicon */}
+        <link rel="icon" href="/img/aulavirtual.png" />
+        
+      </Head>
       <body>
         <Navbar/>
         {children}
